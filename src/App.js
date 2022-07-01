17e9da30e54e58150477e915504cf5cda561cd48
Alfa <900123@telkom.co.id>
2022-07-01 11:28:20 +0700

    initial project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
 import './App.css';
+import io from 'socket.io-client'
 
 function App() {
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] =useState([])
+  const [socket, setSocket] = useState(null)
+
+  useEffect(()=>{
+    const resultSocket = io('http://localhost:4000')
+
+    setSocket(resultSocket)
+    resultSocket.on('messageBE',(data)=>{
+      setMessages((current)=>[...current, data])
+    })
+  },[])
+  
+  const handleSendMessage = ()=>{
+    socket.emit('message', message)
+    setMessage('')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {messages.map((item)=>(
+          <li>{item.message} - {new Date(item.date).getHours()}:{new Date(item.date).getMinutes()}</li>
+        ))}
+      </ul>
+      <input type="text" value={message} name="message" id="message" onChange={(e)=> setMessage(e.target.value)} />
+      <button onClick={handleSendMessage}>send message</button>
     </div>
   );
 }
